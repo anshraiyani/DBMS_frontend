aa673233239185293bc9093f5bf6e1db698eeebf
Ansh Raiyani <anshraiyani13@gmail.com>
2023-04-14 01:22:41 +0530

    completed enroll form

--- student-script.js
+++ student-script.js
@@ -1,5 +1,23 @@
+const cancelBtn = document.querySelector('.cancel');
+const cancelBtn2 = document.querySelector('.cancel2');
+
 const registerBtn = document.querySelector('.register-btn');
-const form = document.querySelector('form')
+const registerForm = document.querySelector('.register-form');
+
+const enrollBtn = document.querySelector('.enroll-btn');
+const enrollForm = document.querySelector('.enroll-form');
+
 registerBtn.addEventListener('click',()=>{
-    form.style.display="";
-})
+    registerForm.style.display="";
+});
+
+enrollBtn.addEventListener('click',()=>{
+    enrollForm.style.display="";
+});
+
+cancelBtn.addEventListener('click',()=>{
+    registerForm.style.display="none";
+});
+cancelBtn2.addEventListener('click',()=>{
+    enrollForm.style.display="none";
+});
